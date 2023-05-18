ed152e95c2ac655f37d5ebc4bc49a6de5679bff5
Diego sales <diegofelipesales23@gmail.com>
2023-05-18 14:23:12 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, Text, View, TouchableOpacity } from "react-native";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blurBg from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg"; //vira um component, por causa do pacj
+import NLWlogo from "./src/assets/nlw-spacetime-logo.svg";
+import { styled } from "nativewind";
+
+const StyleStripes = styled(Stripes); //somente se for estilizar
+
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className='bg-zinc-950 flex-1 items-center justify-center'>
-      <Text className='text-2xl text-cyan-100'>meu novo APP!</Text>
+    <ImageBackground
+      source={blurBg}
+      className="bg-gray-900 flex-1 items-center relative px-8 py-10"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyleStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWlogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+          // onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
 }
-
